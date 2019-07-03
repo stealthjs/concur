647773035d6edbd97df96ba326585fd7ed230d38
Jason <stealthjs@icloud.com>
2019-07-03 19:56:24 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	jobs := make(chan int, 50)
+	results := make(chan int, 50)
+
+	go worker(jobs, results)
+
+	for i := 0; i < 45; i++ {
+		jobs <- i
+	}
+	close(jobs)
+
+	for j := 0; j < 45; j++ {
+		fmt.Println(<-results)
+	}
+	fmt.Println("Finished")
+}
+
+func worker(jobs <-chan int, results chan<- int) {
+	for n := range jobs {
+		results <- fib(n)
+	}
+}
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
